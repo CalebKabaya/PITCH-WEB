091208fdb4b88583a2c3f715f00a6b53a76c9ddb
calebkabaya <calebkabaya@gmail.com>
2022-05-07 18:10:26 +0300

    ft:added the function to update user profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import DataRequired
+# from wtforms.validators import Required
+
+
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
     submit = SubmitField('Submit')
