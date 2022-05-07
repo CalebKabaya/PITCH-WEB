e5243a3228a33da49f99242724fb6984a20f1ef5
calebkabaya <calebkabaya@gmail.com>
2022-05-07 20:12:38 +0300

    ft:created the pithc form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import DataRequired
 # from wtforms.validators import Required
 
@@ -7,3 +7,9 @@ from wtforms.validators import DataRequired
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
     submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    category = SelectField('Category', choices=[('Events','Events'),('Job','Job'),('Advertisement','Advertisement')],validators=[Required()])
+    post = TextAreaField('Your Pitch', validators=[DataRequired()])
+    submit = SubmitField('Pitch')
