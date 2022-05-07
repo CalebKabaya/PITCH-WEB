3551aa5de45dc7edc5fce4fd061b94e37656fed1
calebkabaya <calebkabaya@gmail.com>
2022-05-07 17:57:40 +0300

    ft:created the function to display user profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
-from flask import render_template
 from . import main
 from flask_login import login_required
-
+from flask import render_template,request,redirect,url_for,abort
+from ..models import  User
 
 @main.route('/')
 def index():
@@ -11,3 +11,12 @@ def index():
     """
 
     return render_template('index.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
