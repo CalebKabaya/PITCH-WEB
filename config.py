40bd71c89b192f86384aebc8c57e9ff9a8772c51
calebkabaya <calebkabaya@gmail.com>
2022-05-08 21:45:11 +0300

    deploying to heroku 4

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
+# import re
+
+# uri = os.getenv("DATABASE_URL")  # or other relevant config var
+# if uri.startswith("postgres://"):
+#     uri = uri.replace("postgres://", "postgresql://", 1)
+# # rest of connection code using the connection string `uri`
 
 class Config:
     '''
