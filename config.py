067e1b15ced160b1d24aa089fe2ffaf7b34925d6
calebkabaya <calebkabaya@gmail.com>
2022-05-09 21:31:17 +0300

    deploy t

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config():
    '''
    SECRET_KEY=os.environ.get('SECRET_KEY')
    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Mbuguack@localhost/pitches'
-   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+   # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
    UPLOADED_PHOTOS_DEST ='app/static/photos'
    #simple mde configuration
