014c9fefaeff5706aba992fe6c46f55128a9ebc9
calebkabaya <calebkabaya@gmail.com>
2022-05-09 08:13:59 +0300

    deploying 1

--- config.py
+++ config.py
@@ -41,7 +41,9 @@ class ProdConfig(Config):
    '''
    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Mbuguack@localhost/watchlist'
    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-   pass
+   SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL')
+
+   
 
 class DevConfig(Config):
 
