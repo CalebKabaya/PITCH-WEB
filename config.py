bbbbf7a09a1c9c240cb54a2f152917915d5ea2f5
calebkabaya <calebkabaya@gmail.com>
2022-05-09 17:05:15 +0300

    deployong to heroku

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
    '''
    General configuration parent class
    '''
-
+   DATABASE_URL = os.environ['DATABASE_URL']
    SECRET_KEY=os.environ.get('SECRET_KEY')
 #    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Mbuguack@localhost/pitches'
    UPLOADED_PHOTOS_DEST ='app/static/photos'
@@ -44,6 +44,8 @@ class ProdConfig(Config):
    '''
    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Mbuguack@localhost/watchlist'
    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+   DATABASE_URL = os.environ['DATABASE_URL']
+
    pass
 
 class DevConfig(Config):
